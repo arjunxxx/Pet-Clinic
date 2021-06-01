51b1c6aa958635797fd2dd004d6d752f285caf01
arjunxxx <arjunrajeev1@gmail.com>
2021-06-01 22:54:13 +0530

    Speciality service and implementations added

---
+++ Pet-Clinic/pet-clinic-data/src/main/java/com/technolearns/services/SpecialityService.java
@@ -0,0 +1,12 @@
+package com.technolearns.services;
+
+import java.util.Set;
+
+import com.technolearns.model.Owner;
+import com.technolearns.model.Speciality;
+import com.technolearns.model.Vet;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+	
+
+}
---
+++ Pet-Clinic/pet-clinic-data/src/main/java/com/technolearns/services/map/SpecialityServiceMap.java
@@ -0,0 +1,38 @@
+package com.technolearns.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.technolearns.model.Speciality;
+import com.technolearns.services.SpecialityService;
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService{
+
+	@Override
+	public Speciality save(Speciality object) {
+		return super.save(object);
+	}
+
+	@Override
+	public Set<Speciality> findAll() {
+		return super.findAll();
+	} 
+ 
+	@Override
+	public Speciality findById(Long id) {
+		return super.findById(id);
+	}
+    
+	@Override
+	public void delete(Speciality object) {
+		super.delete(object);
+	}
+	
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	
+}
