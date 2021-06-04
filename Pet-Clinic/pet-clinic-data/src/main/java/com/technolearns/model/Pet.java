993221605de3a2a5135fa30e6cbc6431e6b89de7
arjunxxx <arjunrajeev1@gmail.com>
2021-06-04 08:36:51 +0530

    jpa modeling

--- Pet-Clinic/pet-clinic-data/src/main/java/com/technolearns/model/Pet.java
+++ Pet-Clinic/pet-clinic-data/src/main/java/com/technolearns/model/Pet.java
@@ -2,16 +2,34 @@ package com.technolearns.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="pets")
 public class Pet extends BaseEntity{
 	
+	@ManyToOne
+	@JoinColumn(name="owner_id")
 	private Owner owner;
 	
+	@Column(name="birth_day")
 	private LocalDate birthday;
 	
+	@ManyToOne
+	@JoinColumn(name="pet_type_id")
 	private PetType petType;
 	
+	@Column(name="name")
 	private String name;
 
+	
 	public Owner getOwner() {
 		return owner;
 	}
