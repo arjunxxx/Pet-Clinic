6ea159c38d7e023c20ced8a4ab7a72e681d04dc2
arjunxxx <arjunrajeev1@gmail.com>
2021-06-04 08:20:31 +0530

    jpa model

--- Pet-Clinic/pet-clinic-data/src/main/java/com/technolearns/model/Person.java
+++ Pet-Clinic/pet-clinic-data/src/main/java/com/technolearns/model/Person.java
@@ -1,9 +1,15 @@
 package com.technolearns.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass //To specify that its just a parent class and do not need separate column in DB. It just gives attributes to sub classes.
 public class Person extends BaseEntity {
 	
+	@Column(name="first_name")
 	private String firstname;
 	
+	@Column(name="last_name")
 	private String lastname;
 
 	public String getFirstname() {
