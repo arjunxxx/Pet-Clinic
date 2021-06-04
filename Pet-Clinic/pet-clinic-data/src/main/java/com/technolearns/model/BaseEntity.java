23e2426affc02da8adc459345b287c94060292e1
arjunxxx <arjunrajeev1@gmail.com>
2021-06-04 08:11:41 +0530

    converted to JPA model

--- Pet-Clinic/pet-clinic-data/src/main/java/com/technolearns/model/BaseEntity.java
+++ Pet-Clinic/pet-clinic-data/src/main/java/com/technolearns/model/BaseEntity.java
@@ -2,6 +2,13 @@ package com.technolearns.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 	
 	/**
@@ -9,6 +16,8 @@ public class BaseEntity implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
