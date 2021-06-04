ce168fc9d5dc19532498da9f4e4a90ca6123449c
arjunxxx <arjunrajeev1@gmail.com>
2021-06-04 09:02:03 +0530

    table annotation added

--- Pet-Clinic/pet-clinic-data/src/main/java/com/technolearns/model/Visit.java
+++ Pet-Clinic/pet-clinic-data/src/main/java/com/technolearns/model/Visit.java
@@ -2,14 +2,13 @@ package com.technolearns.model;
 
 import java.time.LocalDate;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
+import javax.persistence.Table;
 @Entity
+@Table(name="visits")
 public class Visit extends BaseEntity {
 	
 
